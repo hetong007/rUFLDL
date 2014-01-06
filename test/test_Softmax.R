b93d5ffa0d80ebd99e95375c1d22c8ed9060ceb3
hetong007 <hetong007@gmail.com>
2014-01-06 16:16:40 +0800

    modified requirement

--- test/test_Softmax.R
+++ test/test_Softmax.R
@@ -3,7 +3,7 @@ x = matrix(rnorm(10000),ncol=5)
 y = x[,1]-x[,2]*x[,4]+x[,3]^2
 y = findInterval(y,quantile(y,c(0.3,0.6)))
 
-
+require(Matrix)
 model = Softmax(x[1:1000,],y[1:1000],0.1,0.01)
 
 fitted = model[[2]]
