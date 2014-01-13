87f6b91d4d2aaccc06b1be2c4696833b1fe9c2be
hetong007 <hetong007@gmail.com>
2014-01-13 16:57:35 +0800

    modified parameters in MNIST

--- MNIST/MNIST_DeepNetworks.R
+++ MNIST/MNIST_DeepNetworks.R
@@ -2,9 +2,10 @@ load('MNIST/data/train.rda')
 x = as.matrix(train[1:2000,-1])
 y = train[1:2000,1]
 
-model = DeepNetworks(x,y,nodes_1=500,maxStep_1=200,sparsity_1=TRUE,
-                     nodes_2=500,maxStep_2=200,sparsity_2=TRUE,maxStep_3=3000,
-                     alpha=0.1,lambda=3e-3,maxStep_back = 200)
+model = DeepNetworks(x[1:1000,],y[1:1000],
+                     nodes_1=500,maxStep_1=500,sparsity_1=TRUE,
+                     nodes_2=500,maxStep_2=500,sparsity_2=TRUE,maxStep_3=10000,
+                     alpha=0.1,lambda=3e-3,maxStep_back = 1000)
 W = model[[1]]
 b = model[[2]]
 fitted = ForwardPropagation(x[1:1000,],W,b,'single')
@@ -17,6 +18,8 @@ pred = apply(pred,1,which.max)-1
 table(as.factor(fitted),as.factor(y[1:1000]))
 sum(diag(table(as.factor(fitted),as.factor(y[1:1000]))))/
     sum(table(as.factor(fitted),as.factor(y[1:1000])))
+#expected to be 0.99
 table(as.factor(pred),as.factor(y[1001:2000]))
 sum(diag(table(as.factor(pred),as.factor(y[1001:2000]))))/
     sum(table(as.factor(pred),as.factor(y[1001:2000])))
+#expected to be 0.848
